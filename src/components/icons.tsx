e04f20e6e21aa51a4460e52ea966434a81a19284
Brendon Oleghe (Maestro) <imudiabrendon@gmail.com>
2025-09-29 10:08:17 +0000

    Try fixing this error: `Console Error: Invalid DOM property `stroke-widt

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -88,9 +88,9 @@ export const Icons = {
       viewBox="0 0 24 24" 
       fill="none" 
       stroke="currentColor" 
-      stroke-width="2" 
-      stroke-linecap="round" 
-      stroke-linejoin="round" 
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
       {...props}
     >
       <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 13.5a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7z" />
@@ -122,9 +122,9 @@ export const Icons = {
       viewBox="0 0 24 24" 
       fill="none" 
       stroke="currentColor" 
-      stroke-width="2" 
-      stroke-linecap="round" 
-      stroke-linejoin="round"
+      strokeWidth="2" 
+      strokeLinecap="round" 
+      strokeLinejoin="round"
       {...props}
     >
       <path d="M12 2a10 10 0 1 0 10 10A10 10 0 0 0 12 2zm0 13.5a3.5 3.5 0 1 1 0-7 3.5 3.5 0 0 1 0 7z" />
