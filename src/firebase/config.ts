b4eb10092bd9554007eaa3d97bd83e3b53a0445f
Brendon Oleghe (Maestro) <imudiabrendon@gmail.com>
2025-10-04 02:59:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6286417362-ef95c",
+  "appId": "1:116357963805:web:dbe9e9c4da7e51c25626f4",
+  "apiKey": "AIzaSyBv8Nl2Q1m4w-Dzh8R7Gnyng1nEdXgMNqg",
+  "authDomain": "studio-6286417362-ef95c.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "116357963805"
+};
